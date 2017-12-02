4c3ea099dba5ef7bd8e778e1a3de5ad508c30455
Nao YONASHIRO <owan.orisano@gmail.com>
2017-12-03 00:57:24 +0900

    refactor: remove effect to global state

--- bqspec/loader.py
+++ bqspec/loader.py
@@ -4,13 +4,14 @@ from __future__ import unicode_literals
 import ruamel.yaml
 import six
 
+yaml = ruamel.yaml.YAML(typ="safe")
 if six.PY2:
 
     def _construct_yaml_str(self, node):
         return self.construct_scalar(node)
 
-    ruamel.yaml.SafeLoader.add_constructor("tag:yaml.org,2002:str", _construct_yaml_str)
+    yaml.constructor.add_constructor("tag:yaml.org,2002:str", _construct_yaml_str)
 
 
 def load_yaml(stream):
-    return ruamel.yaml.safe_load(stream)
+    return yaml.load(stream)
