da48dd854841df8e358cd1dcabed484652260054
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2019-10-23 14:28:43 +0900

    feat: add support python version

--- setup.py
+++ setup.py
@@ -87,6 +87,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
